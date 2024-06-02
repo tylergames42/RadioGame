fa0ef648630781cdfa4e92e6f9909f77dfa342b3
tylergames42 <51301871+tylergames42@users.noreply.github.com>
2024-06-02 04:30:04 -0500

    Updated radio shitt

--- scripts/radio.gd
+++ scripts/radio.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 
 @export var SIGNAL_MANAGER : Node3D
-@export var FREQUENCY = 10.0
+@export var STARTING_FREQUENCY = 10.0
 @export var STATIC : AudioStream
 @export_range(-20.0, 0.0, 1.0) var STATIC_VOLUME : float = -10.0
 
@@ -17,8 +17,8 @@ var reciever_signal_strength = 0.0
 var connected_signal : SignalTransmitter
 var signal_strengths : float
 
-var tune_speed : float = 0.0
-var anim_time : float = 0.0
+var frequency : float = 0.0
+var desired_frequency : float = 33.0
 
 func _ready():
 	add_child(static_player)
@@ -28,31 +28,40 @@ func _ready():
 	static_player.volume_db = STATIC_VOLUME
 
 func _input(event):
-	if event.is_action_pressed("tune_up"):
+	if frequency < 99.9 and event.is_action_pressed("tune_up"):
 		if Input.is_action_pressed("tune_modifiy"):
-			tune_speed = 0.65
+			desired_frequency += 10
 		else:
-			tune_speed += 0.01
-		if FREQUENCY < 99.9:
-			static_player.play()
-			static_player.seek(randf_range(0.0, 10.0))
-	if event.is_action_pressed("tune_down"):
+			desired_frequency += 0.1
+			
+		if desired_frequency > 99.9:
+				desired_frequency = 99.9
+				
+		static_player.play()
+		static_player.seek(randf_range(0.0, 10.0))
+	if frequency > 0.1 and event.is_action_pressed("tune_down"):
 		if Input.is_action_pressed("tune_modifiy"):
-			tune_speed = -0.65
+			desired_frequency -= 10
 		else:
-			tune_speed -= 0.01
-		if FREQUENCY > 0.1:
-			static_player.play()
-			static_player.seek(randf_range(0.0, 10.0))
+			desired_frequency -= 0.1
+			
+		if desired_frequency < 0.1:
+				desired_frequency = 0.1
+				
+		static_player.play()
+		static_player.seek(randf_range(0.0, 10.0))
 
 func _process(delta):
-	tune_speed = lerp(tune_speed, 0.0, delta * 5)
-		
+	print(desired_frequency)
+	
+	var freq_diff = (desired_frequency - frequency)
+	var tune_speed = clamp(desired_frequency - frequency, -1, 1) * 0.5
+	
 	if ANIM_TREE["parameters/tune_anim/time"] != null:
 		ANIM_TREE["parameters/tune_speed/scale"] = tune_speed
-		anim_time = ANIM_TREE["parameters/tune_anim/time"]
-		FREQUENCY = remap(anim_time, 0.0, 1.25, 0.1, 100.0)
-		Global.frequency = FREQUENCY
+		var anim_time = ANIM_TREE["parameters/tune_anim/time"]
+		frequency = remap(anim_time, 0.0, 1.25, 0.1, 100.0)
+		Global.frequency = frequency
 		update_visuals()
 	#
 	
@@ -70,7 +79,7 @@ func _process(delta):
 func find_signals():
 	for sig in SIGNAL_MANAGER.get_children():
 		if sig is SignalTransmitter:
-			var freq_diff = absf(sig.FREQUENCY - FREQUENCY) #Get difference in our frequency and transmitter's
+			var freq_diff = absf(sig.FREQUENCY - frequency) #Get difference in our frequency and transmitter's
 			freq_diff = clampf(freq_diff, 0.0, 1.0) #Clamp difference to value between 0.0 and 1.0
 			var distance = global_position.distance_to(sig.global_position) #Get distance to transmitter
 			if (freq_diff < 1.0) && (distance <= sig.RANGE_PARTIAL): #Start recieving signal if in range and freq
@@ -99,5 +108,5 @@ func tune(amount : float):
 		return
 					
 func update_visuals():
-	MAT_OSC.set_shader_parameter("freq", FREQUENCY)
+	MAT_OSC.set_shader_parameter("freq", frequency)
 	#MAT_STRENGTH_IND.set_shader_parameter("strength", reciever_signal_strength)
--- scripts/update_display.gd
+++ scripts/update_display.gd
@@ -14,3 +14,6 @@ func _ready():
 func _input(event):
 	anim_player.play("radio_tune", -1, 0.0)
 	anim_player.seek(remap(Global.frequency, 1.0, 100.0, 0.0, 2.0))
+	
+	for body in get_overlapping_bodies():
+		if body.name = ""
